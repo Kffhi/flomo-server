3d09a1515cb608651a63881b9e1bf79bd6b9cce3
wuhao <13979215061@163.com>
2022-03-31 20:24:56 +0800

    feat: 基础的JSON文件读写

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const bodyParser = require('body-parser')
+const fs = require("fs")
+const path = require("path");
 
 const hostname = "localhost"
 const port = process.env.PORT || 3000
@@ -10,7 +12,8 @@ app.use(bodyParser.json())
 
 const router = express.Router()
 
-registerSimpleRouter()
+registerGetMockRouter()
+registerSetMockRouter()
 
 app.use(router)
 
@@ -18,10 +21,26 @@ module.exports = app.listen(port, () => {
     console.log(`Server listening on http://${hostname}:${port}, Ctrl+C to stop`)
 })
 
-function registerSimpleRouter() {
-    router.get('/simple/get', function (req, res) {
+function registerGetMockRouter() {
+    router.get('/getMock', function (req, res) {
+        const data = fs.readFileSync(path.resolve(__dirname, './database/mock.json'), 'utf-8')
         res.json({
-            msg: `hello world`
+            msg: `hello world`,
+            data: JSON.parse(data)
+        })
+    })
+}
+
+function registerSetMockRouter() {
+    router.get('/setMock', function (req, res) {
+        let data = fs.readFileSync(path.resolve(__dirname, './database/mock.json'), 'utf-8')
+        data = JSON.parse(data)
+        data.setMes = req.query.msg
+        data = JSON.stringify(data)
+        fs.writeFile(path.resolve(__dirname, './database/mock.json'), data, () => {
+            res.json({
+                msg: `OK`
+            })
         })
     })
 }
